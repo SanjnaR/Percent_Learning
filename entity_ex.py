5564b4fa6e8ad2bec6fe66411c71dbcf460bad8f
Jessica Lee <jessicalee@Jessicas-MacBook-Pro.local>
2018-09-16 01:08:35 -0400

    added question gen file

--- entity_ex.py
+++ entity_ex.py
@@ -54,11 +54,11 @@ def entities_text(text):
     for entity in entities:
         print('=' * 20)
         print(u'{:<16}: {}'.format('name', entity.name))
-        # print(u'{:<16}: {}'.format('type', entity_type[entity.type]))
-        # print(u'{:<16}: {}'.format('metadata', entity.metadata))
-        # print(u'{:<16}: {}'.format('salience', entity.salience))
-        # print(u'{:<16}: {}'.format('wikipedia_url',
-        #       entity.metadata.get('wikipedia_url', '-')))
+        print(u'{:<16}: {}'.format('type', entity_type[entity.type]))
+        print(u'{:<16}: {}'.format('metadata', entity.metadata))
+        print(u'{:<16}: {}'.format('salience', entity.salience))
+        print(u'{:<16}: {}'.format('wikipedia_url',
+              entity.metadata.get('wikipedia_url', '-')))
 
 
 entities_text(load_words())
