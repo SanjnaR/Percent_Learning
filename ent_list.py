98de70204fb9e59c5cc67e3a6e88b05cf5f45a07
Jessica Lee <jessicalee@Jessicas-MacBook-Pro.local>
2018-09-16 01:43:18 -0400

    question generated integrated

--- ent_list.py
+++ ent_list.py
@@ -51,7 +51,21 @@ def entities_text(text):
     for entity in entities:
         ent_list.append((entity.name, entity_type[entity.type]))
 
-    return ent_list
+    return q_gen(ent_list)
+
+def q_gen(q_data):
+    """
+    inputs q_data = list of tuples[(word, entity type)...()]
+    @returns q = list of questions from generated entity type
+    """
+    q = []
+    q_template = {'UNKNOWN': None, 'PERSON':"Who was ", 'LOCATION':"Where is ", 'ORGANIZATION': "What was the ",
+                    'EVENT': "What was the ", 'WORK_OF_ART': "What was the ", 'CONSUMER_GOOD': "What was the ", 'OTHER': "What's the importance of "}
+    # Person, detect if common or proper to see common + "the "
+
+    for t in q_data: 
+        q.append(q_template[t[1]] + t[0] + "?")
+    return q
 
 
 print(entities_text(load_words()))
